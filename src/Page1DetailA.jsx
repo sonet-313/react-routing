c2345fa177c836b92a81428bb48b3c7b3c61ce5e
Sonetaka Mikihiro <60766275+sonet-313@users.noreply.github.com>
2022-03-04 09:40:49 +0900

    Initial commit

---
+++ src/Page1DetailA.jsx
@@ -0,0 +1,15 @@
+import { useLocation, useHistory } from "react-router-dom";
+export const Page1DetailA = () => {
+  const location = useLocation();
+  console.log(location.state);
+
+  const history = useHistory();
+
+  const onClickBack = () => history.goBack();
+  return (
+    <div>
+      <h1>Page1DetailAページです</h1>
+      <button onClick={onClickBack}>戻る</button>
+    </div>
+  );
+};
